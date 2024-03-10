43ab5f4cbc1006230c09915012f6ce1103ce58a9
Syed Aoun Abbas Naqvi <syedaoun.dev@gmail.com>
2024-03-10 22:07:30 +0500

    initail commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View, Alert, Button } from 'react-native';
+
+import * as ImagePicker from "expo-image-picker";
+import { useState } from 'react';
+
 
 export default function App() {
+
+
+  const [image, setImage] = useState('')
+
+
+
+
+  const pickImage = async () => {
+    try {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission to access media library is required.");
+        console.log("Permission to access media library is required.");
+        return;
+      }
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        aspect: [7, 7],
+        quality: 1,
+        allowsEditing: true
+      });
+
+      if (!result.canceled) {
+
+        console.log(result)
+        setImage(result.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {image != '' &&
+        (<Image source={{ uri: image }} style={styles.image} />)
+      }
+
+      <Button title='Pick Image' onPress={() => pickImage()} />
     </View>
   );
 }
@@ -17,4 +57,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 200,
+    height: 300,
+    resizeMode: 'contain'
+  }
 });
